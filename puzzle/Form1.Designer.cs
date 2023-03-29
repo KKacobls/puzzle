a84ce11ba327835f9030ea86a4889be1c8090192
KKacobls <kkacobls@gmail.com>
2023-03-29 10:52:33 +0800

    'F'

---
+++ puzzle/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            b1 = new Button();
+            imageList1 = new ImageList(components);
+            b2 = new Button();
+            b3 = new Button();
+            b6 = new Button();
+            b5 = new Button();
+            b4 = new Button();
+            b9 = new Button();
+            b8 = new Button();
+            b7 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            SuspendLayout();
+            // 
+            // b1
+            // 
+            b1.ImageIndex = 0;
+            b1.ImageList = imageList1;
+            b1.Location = new Point(162, 118);
+            b1.Name = "b1";
+            b1.Size = new Size(100, 100);
+            b1.TabIndex = 0;
+            b1.UseVisualStyleBackColor = true;
+            b1.Click += b1_Click;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth8Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "1.png");
+            imageList1.Images.SetKeyName(1, "2.png");
+            imageList1.Images.SetKeyName(2, "3.png");
+            imageList1.Images.SetKeyName(3, "4.png");
+            imageList1.Images.SetKeyName(4, "5.png");
+            imageList1.Images.SetKeyName(5, "6.png");
+            imageList1.Images.SetKeyName(6, "7.png");
+            imageList1.Images.SetKeyName(7, "8.png");
+            imageList1.Images.SetKeyName(8, "pic.png");
+            imageList1.Images.SetKeyName(9, "white.png");
+            // 
+            // b2
+            // 
+            b2.ImageIndex = 1;
+            b2.ImageList = imageList1;
+            b2.Location = new Point(256, 118);
+            b2.Name = "b2";
+            b2.Size = new Size(100, 100);
+            b2.TabIndex = 1;
+            b2.UseVisualStyleBackColor = true;
+            b2.Click += b2_Click;
+            // 
+            // b3
+            // 
+            b3.ImageIndex = 2;
+            b3.ImageList = imageList1;
+            b3.Location = new Point(352, 118);
+            b3.Name = "b3";
+            b3.Size = new Size(100, 100);
+            b3.TabIndex = 2;
+            b3.UseVisualStyleBackColor = true;
+            b3.Click += b3_Click;
+            // 
+            // b6
+            // 
+            b6.ImageIndex = 5;
+            b6.ImageList = imageList1;
+            b6.Location = new Point(352, 211);
+            b6.Name = "b6";
+            b6.Size = new Size(100, 100);
+            b6.TabIndex = 5;
+            b6.UseVisualStyleBackColor = true;
+            b6.Click += b6_Click;
+            // 
+            // b5
+            // 
+            b5.ImageIndex = 4;
+            b5.ImageList = imageList1;
+            b5.Location = new Point(256, 211);
+            b5.Name = "b5";
+            b5.Size = new Size(100, 100);
+            b5.TabIndex = 4;
+            b5.UseVisualStyleBackColor = true;
+            b5.Click += b5_Click;
+            // 
+            // b4
+            // 
+            b4.ImageIndex = 3;
+            b4.ImageList = imageList1;
+            b4.Location = new Point(162, 211);
+            b4.Name = "b4";
+            b4.Size = new Size(100, 100);
+            b4.TabIndex = 3;
+            b4.UseVisualStyleBackColor = true;
+            b4.Click += b4_Click;
+            // 
+            // b9
+            // 
+            b9.ImageIndex = 8;
+            b9.ImageList = imageList1;
+            b9.Location = new Point(352, 307);
+            b9.Name = "b9";
+            b9.Size = new Size(100, 100);
+            b9.TabIndex = 8;
+            b9.UseVisualStyleBackColor = true;
+            b9.Click += b9_Click;
+            // 
+            // b8
+            // 
+            b8.ImageIndex = 7;
+            b8.ImageList = imageList1;
+            b8.Location = new Point(256, 307);
+            b8.Name = "b8";
+            b8.Size = new Size(100, 100);
+            b8.TabIndex = 7;
+            b8.UseVisualStyleBackColor = true;
+            b8.Click += button8_Click;
+            // 
+            // b7
+            // 
+            b7.ImageIndex = 6;
+            b7.ImageList = imageList1;
+            b7.Location = new Point(162, 307);
+            b7.Name = "b7";
+            b7.Size = new Size(100, 100);
+            b7.TabIndex = 6;
+            b7.UseVisualStyleBackColor = true;
+            b7.Click += b7_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(485, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 9;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(549, 62);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 10;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(617, 62);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 15);
+            label3.TabIndex = 11;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(485, 114);
+            label4.Name = "label4";
+            label4.Size = new Size(42, 15);
+            label4.TabIndex = 14;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(549, 114);
+            label5.Name = "label5";
+            label5.Size = new Size(42, 15);
+            label5.TabIndex = 13;
+            label5.Text = "label5";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(617, 114);
+            label6.Name = "label6";
+            label6.Size = new Size(42, 15);
+            label6.TabIndex = 12;
+            label6.Text = "label6";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(485, 171);
+            label7.Name = "label7";
+            label7.Size = new Size(42, 15);
+            label7.TabIndex = 17;
+            label7.Text = "label7";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(549, 171);
+            label8.Name = "label8";
+            label8.Size = new Size(42, 15);
+            label8.TabIndex = 16;
+            label8.Text = "label8";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(617, 171);
+            label9.Name = "label9";
+            label9.Size = new Size(42, 15);
+            label9.TabIndex = 15;
+            label9.Text = "label9";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Microsoft JhengHei UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(485, 224);
+            label10.Name = "label10";
+            label10.Size = new Size(0, 41);
+            label10.TabIndex = 18;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label11.Location = new Point(458, 365);
+            label11.Name = "label11";
+            label11.Size = new Size(109, 30);
+            label11.TabIndex = 19;
+            label11.Text = "直接完成";
+            label11.Click += label11_Click;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label12.Location = new Point(458, 323);
+            label12.Name = "label12";
+            label12.Size = new Size(109, 30);
+            label12.TabIndex = 20;
+            label12.Text = "重新挑戰";
+            label12.Click += label12_Click;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label13.Location = new Point(583, 323);
+            label13.Name = "label13";
+            label13.Size = new Size(0, 30);
+            label13.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1244, 578);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(label4);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(b9);
+            Controls.Add(b8);
+            Controls.Add(b7);
+            Controls.Add(b6);
+            Controls.Add(b5);
+            Controls.Add(b4);
+            Controls.Add(b3);
+            Controls.Add(b2);
+            Controls.Add(b1);
+            Margin = new Padding(2);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button b1;
+        private Button b2;
+        private Button b3;
+        private Button b6;
+        private Button b5;
+        private Button b4;
+        private Button b9;
+        private Button b8;
+        private Button b7;
+        private ImageList imageList1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+    }
+}
---
+++ puzzle/Form1.cs
@@ -0,0 +1,88 @@
+namespace puzzle
+{
+    public partial class Form1 : Form
+    {
+
+        public void main()
+        {
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void myTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+        public void updata()
+        {
+
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            updata();
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+
+            updata();
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+
+            updata();
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+
+            updata();
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+
+            updata();
+        }
+
+        private void b4_Click(object sender, EventArgs e)
+        {
+
+            updata();
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+
+            updata();
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+
+            updata();
+        }
+
+        private void b9_Click(object sender, EventArgs e)
+        {
+
+            updata();
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
